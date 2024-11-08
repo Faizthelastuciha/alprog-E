f550029a78ec6647f69658a51dadc97c1ca87055
Faizthelastuciha <184088609+Faizthelastuciha@users.noreply.github.com>
2024-11-08 09:57:28 +0800

    Add files via upload

---
+++ prog6.py
@@ -0,0 +1,25 @@
+#variable assigment value#
+x = 9
+y = 6
+print("nilai dari x adalah : ",y)
+
+# aturan penamaan variable
+luas = 11#
+faiz_uciha = 125#
+ObitoX= 7
+obitoX = 50
+itaci55 = 20
+print("nilai dari Obito  adalah:,", itaci55)
+
+
+a = 4
+print("tipe data dari a:", type  (a))
+
+b = 6.9
+print("tipe data dari b:", type  (b))
+
+c = "gacor"
+print("tipe data dari c:", type  (c))
+
+d = False
+print("tipe data dari d:", type  (d))
